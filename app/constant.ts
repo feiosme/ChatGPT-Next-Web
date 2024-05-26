cee8d4893a7d24496ebc140d3152ab2b07756bde
feiosme <1327979498@qq.com>
2024-05-26 23:15:55 +0800

    add models

--- app/constant.ts
+++ app/constant.ts
@@ -11,6 +11,9 @@ export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
 export const DEFAULT_API_HOST = "https://api.nextchat.dev";
 export const OPENAI_BASE_URL = "https://api.openai.com";
 export const ANTHROPIC_BASE_URL = "https://api.anthropic.com";
+export const QWEN_BASE_URL = "https://dashscope.aliyuncs.com/compatible-mode/v1";
+export const YI_BASE_URL = "https://api.lingyiwanwu.com/v1";
+export const ZHIPU_BASE_URL = "https://open.bigmodel.cn/api/paas/v4/chat/completions";
 
 export const GEMINI_BASE_URL = "https://generativelanguage.googleapis.com/";
 
@@ -136,6 +139,35 @@ export const KnowledgeCutOffDate: Record<string, string> = {
   "gemini-pro-vision": "2023-12",
 };
 
+const qwenModels = [
+  "qwen-max",
+  "qwen-max-0428",
+  "qwen-long",
+  "qwen-max-longcontext",
+  "qwen-plus",
+  "qwen-turbo",
+  "bailian-summary",
+  "qwen1.5-110b-chat",
+  "qwen-72b-chat",
+  "qwen1.5-72b-chat",
+  "qwen1.5-32b-chat",
+  "qwen1.5-14b-chat",
+  "qwen1.5-7b-chat"
+]
+
+const yiModels = [
+  "yi-large",
+  "yi-large-rag",
+  "yi-medium-200k",
+  "yi-vision",
+  "yi-large-turbo"
+]
+
+const zhipuModels = [
+  "glm-4",
+  "glm-3-turbo"
+]
+
 const openaiModels = [
   "gpt-3.5-turbo",
   "gpt-3.5-turbo-1106",
@@ -169,6 +201,33 @@ const anthropicModels = [
 ];
 
 export const DEFAULT_MODELS = [
+  ...qwenModels.map((name) => ({
+    name,
+    avaliable: true,
+    provider: {
+      id: "openai",
+      providerName: "OpenAI",
+      providerType: "openai",
+    },
+  })),
+  ...yiModels.map((name) => ({
+    name,
+    avaliable: true,
+    provider: {
+      id: "openai",
+      providerName: "OpenAI",
+      providerType: "openai",
+    },
+  })),
+  ...zhipuModels.map((name) => ({
+    name,
+    avaliable: true,
+    provider: {
+      id: "openai",
+      providerName: "OpenAI",
+      providerType: "openai",
+    },
+  })),
   ...openaiModels.map((name) => ({
     name,
     available: true,
